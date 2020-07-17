0c52cc9ed8377efe8f806b55bf6a60f962423dd8
tindoann <tindoann@gamil.com>
2020-07-16 22:18:53 -0400

    fix errors in server

---
+++ client/build/precache-manifest.3647fa6ecfa1c170469099ebef9ab6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83012b50319183b87996a4db7c87be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac16b0651c86d47bb237",
+    "url": "/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "ecd4a6802f9121b5ea74",
+    "url": "/static/js/2.d42bace2.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.d42bace2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac16b0651c86d47bb237",
+    "url": "/static/js/main.50e071d9.chunk.js"
+  },
+  {
+    "revision": "415ca9393b46c2cda61b",
+    "url": "/static/js/runtime-main.82891623.js"
+  }
+]);
--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,17 +1,39 @@
-import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
+// import React, { useContext } from 'react';
+// import { Transaction } from './Transaction';
+// import { GlobalContext } from '../context/GlobalState';
 
+// export const TransactionList = () => {
+//   const { transactions } = useContext(GlobalContext);
+
+//   return (
+//     <>
+//       <h3>History</h3>
+//       <ul className="list">
+//         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+//       </ul>
+//     </>
+//   )
+// }
+
+import React, { useContext, useEffect } from 'react';
+import { Transaction } from './Transaction';
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
       </ul>
     </>
   )
 }
+
